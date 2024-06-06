3cfcb855951011b907ce88892bfff965b4f181e7
MuJHEMaTMaRO <prathamsinghchouhan14@gmail.com>
2024-06-06 12:43:12 +0530

    c1

--- src/components/WebinarDes.jsx
+++ src/components/WebinarDes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function WebinarDes() {
   return (
     <div className='h-20 md:h-28 w-full bg-zinc-950 flex justify-center items-center'>
-    <p className='text-[2vh] md:text-[2vw] text-white font-semibold px-5 md:px-10 text-center'>
+    <p className='text-[1.5vh] md:text-[2vw] text-white font-semibold px-5 md:px-10 text-center'>
     Team has also produced many documentary and short films such as for Sarvodaya ke liye Matra Shakti , Padmashri Niranjan Goswami etc. 
     </p>
 </div>
