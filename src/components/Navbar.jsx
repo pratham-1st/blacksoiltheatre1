fecea8f4a383968b3e57024c7e7818e98c715fa3
MuJHEMaTMaRO <prathamsinghchouhan14@gmail.com>
2024-06-06 13:39:24 +0530

    caraousel done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
     <div className="h-[11vh] md:h-[10vw] w-full bg-zinc-900 flex ">
       <div className="h-full w-2/6 flex items-center justify-center p-10 md:p-0 ">
         <div className="flex items-center">
-            <img className="h-[7vh] w-[7vh] md:h-[6vw] md:w-[6vw]" src="src\assets\images\logo.png" alt=""/>
+            <img className="h-[7vh] w-[7vh] md:h-[6vw] md:w-[6vw]" src="https://i.postimg.cc/v82R4TZn/logo.png" alt=""/>
             <h1 className="p-3 text-[1.7vh] md:text-[1.7vw] font-extrabold text-white">BLACK SOIL<br />THEATRE.</h1>
         </div>
       </div>
