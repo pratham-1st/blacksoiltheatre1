b53fc9a8d14012e3e0a9c60b9001f12ea09c9075
MuJHEMaTMaRO <prathamsinghchouhan14@gmail.com>
2024-06-07 12:56:29 +0530

    carausel fix

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,37 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-function Carousel() {
-  return (
+const Carousel = () => {
+    return (
 
-
-<div id="default-carousel" className="relative px-5 py-2 h-auto w-full bg-zinc-900" data-carousel="slide">
-    <div className="relative h-36 overflow-hidden rounded-lg md:h-[28rem]">
-        
-        <div className="hidden duration-3000 ease-in-out" data-carousel-item>
-            <img src="https://i.postimg.cc/6QtbjxQw/Whats-App-Image-2024-06-05-at-23-54-31-96b4c236.jpg" className="absolute object-contain block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
-        </div>
+<div id="default-carousel" className="relative w-full" data-carousel="slide">
+    
+    <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
     
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="https://i.postimg.cc/90KNJ4DN/Whats-App-Image-2024-06-05-at-23-56-02-6149a49b.jpg" className="absolute object-contain block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+            <img src="https://i.postimg.cc/6QtbjxQw/Whats-App-Image-2024-06-05-at-23-54-31-96b4c236.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>        
+        </div>
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src="https://i.postimg.cc/90KNJ4DN/Whats-App-Image-2024-06-05-at-23-56-02-6149a49b.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+     
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src="https://i.postimg.cc/SQv5yPg7/Whats-App-Image-2024-06-05-at-23-56-01-5e9deeb2.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
         </div>
   
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="https://i.postimg.cc/SQv5yPg7/Whats-App-Image-2024-06-05-at-23-56-01-5e9deeb2.jpg" className="absolute object-contain block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
-        </div>
-       
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="https://i.postimg.cc/bwXBntcZ/Whats-App-Image-2024-06-05-at-23-56-01-b52c7100.jpg" className="absolute object-contain block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
-        </div>
-      
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="https://i.postimg.cc/8zhnmzNt/Whats-App-Image-2024-06-05-at-23-56-01-f29e2e50.jpg" className="absolute object-contain block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+            <img src="https://i.postimg.cc/bwXBntcZ/Whats-App-Image-2024-06-05-at-23-56-01-b52c7100.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
         </div>
 
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="https://i.postimg.cc/dtZfD9XM/Whats-App-Image-2024-06-05-at-23-56-02-5eb356a3.jpg" className="absolute object-contain block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+            <img src="https://i.postimg.cc/8zhnmzNt/Whats-App-Image-2024-06-05-at-23-56-01-f29e2e50.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        </div>
+
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src="https://i.postimg.cc/dtZfD9XM/Whats-App-Image-2024-06-05-at-23-56-02-5eb356a3.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
         </div>
     </div>
-   
+  
     <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
         <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
         <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
@@ -39,10 +38,10 @@ function Carousel() {
         <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
         <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
     </div>
-  
-</div>
 
-  )
-}
+        </div>
 
-export default Carousel
+    );
+};
+
+export default Carousel;
